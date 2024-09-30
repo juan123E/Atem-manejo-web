29f55a0dec379e3f9b709d43dd84c20677117ee6
juan_ <juan_@ESTEBANQ>
2024-09-30 11:57:17 -0500

    Add project files.

---
+++ Atem manejo web/Controllers/AtemController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Atem_manejo_web.Controllers
+{
+    public class AtemController : Controller
+    {
+        private readonly AtemService _atemService;
+        public AtemController(AtemService atemService)
+        {
+            _atemService = atemService;
+        }
+        [HttpGet("connect")]
+        public IActionResult ConnectAtem(string ipAddress)
+        {
+            var conexion =_atemService.Connect(ipAddress);
+            if (conexion.Equals("Error"))
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, conexion);
+            }
+            else
+            {
+                return Ok(conexion);
+            }
+        }
+    }
+}
---
+++ Atem manejo web/Services/AtemServicecs.cs
@@ -0,0 +1,37 @@
+﻿using BMDSwitcherAPI;
+using System.Runtime.InteropServices;
+
+public class AtemService
+{
+    private IBMDSwitcherDiscovery switcherDiscovery;
+    private IBMDSwitcher switcher;
+
+    public AtemService()
+    {
+        // Instanciar el objeto de descubrimiento sin usar CoCreateInstance
+        switcherDiscovery = new CBMDSwitcherDiscovery();
+    }
+
+    public String Connect(string ipAddress)
+    {
+        var answer = "";
+
+        try
+        {
+            // Conectar al ATEM usando la dirección IP
+            switcherDiscovery.ConnectTo(ipAddress, out switcher, out _); 
+            Console.WriteLine("Conexión exitosa al ATEM");
+            Console.WriteLine(ipAddress);
+            answer = "Conexión exitosa al ATEM";
+            return answer;
+        }
+        catch (COMException ex)
+        {
+            Console.WriteLine($"Error COM: {ex.Message}, Código HRESULT: {ex.HResult}");
+            Console.WriteLine(ipAddress);
+            answer = "error";
+            return answer;
+        }
+    }
+     
+}
